96049e883d87aecc888ae8c160f12adac2f25f9b
Maksim Ruksha <urpanium@gmail.com>
2022-09-18 21:08:00 +0300

    MP-10 add comments and publication date time fields to Map

--- src/main/java/by/maksimruksha/mapgeneration/entities/Map.java
+++ src/main/java/by/maksimruksha/mapgeneration/entities/Map.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.ZonedDateTime;
 import java.util.List;
 
 @Entity
@@ -14,6 +15,9 @@ public class Map {
 
     @Id
     private Long id;
+
+    @Column(name = "Publication Date Time")
+    private ZonedDateTime publicationDateTime;
 
     @Column(name = "Seed")
     private Long seed;
@@ -28,6 +32,10 @@ public class Map {
     @OneToMany(fetch = FetchType.LAZY)
     @JoinColumn(name = "Likes Ids")
     private List<Like> likes;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "Comments Ids")
+    private List<Comment> comments;
 
     @Column(name = "Preview Link")
     private String previewLink;
