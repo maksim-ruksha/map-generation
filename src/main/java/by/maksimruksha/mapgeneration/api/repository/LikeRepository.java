52ead443581f304368ac8c4dc55c311d73092ec7
Maksim Ruksha <urpanium@gmail.com>
2022-09-19 16:49:22 +0300

    MP-6 minor changes to make application run

--- src/main/java/by/maksimruksha/mapgeneration/api/repository/LikeRepository.java
+++ src/main/java/by/maksimruksha/mapgeneration/api/repository/LikeRepository.java
@@ -1,15 +1,19 @@
 package by.maksimruksha.mapgeneration.api.repository;
 
 import by.maksimruksha.mapgeneration.entities.Like;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
-    @Query("SELECT like FROM Like like WHERE like.map.id =: mapId")
+    /*@Query("SELECT like FROM Like like WHERE like.map.id =: mapId")
     List<Like> getMapLikes(Long mapId);
 
     @Query("SELECT COUNT(Like.id) FROM Like like WHERE like.map.id =: mapId")
-    Long getMapLikeCount(Long mapId);
+    Long getMapLikeCount(Long mapId);*/
+
+    Page<Like> findAllByMap(Pageable pageable, Long mapId);
 }
--- src/main/java/by/maksimruksha/mapgeneration/entities/User.java
+++ src/main/java/by/maksimruksha/mapgeneration/entities/User.java
@@ -12,6 +12,7 @@ import java.util.List;
 @Table(name = "Users")
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "Name")
