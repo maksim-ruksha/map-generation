74758bc7cfc65b19f66662ab4705e16477843b71
Maksim Ruksha <urpanium@gmail.com>
2022-09-21 11:27:14 +0300

    MP-14 minor repositories changes

--- src/main/java/by/maksimruksha/mapgeneration/api/repository/LikeRepository.java
+++ src/main/java/by/maksimruksha/mapgeneration/api/repository/LikeRepository.java
@@ -4,16 +4,8 @@ import by.maksimruksha.mapgeneration.entities.Like;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
-    /*@Query("SELECT like FROM Like like WHERE like.map.id =: mapId")
-    List<Like> getMapLikes(Long mapId);
-
-    @Query("SELECT COUNT(Like.id) FROM Like like WHERE like.map.id =: mapId")
-    Long getMapLikeCount(Long mapId);*/
-
     Page<Like> findAllByMap(Pageable pageable, Long mapId);
+    Long countAllByMap(Long mapId);
 }
--- src/main/java/by/maksimruksha/mapgeneration/api/repository/UserRepository.java
+++ src/main/java/by/maksimruksha/mapgeneration/api/repository/UserRepository.java
@@ -2,6 +2,7 @@ package by.maksimruksha.mapgeneration.api.repository;
 
 import by.maksimruksha.mapgeneration.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 }
