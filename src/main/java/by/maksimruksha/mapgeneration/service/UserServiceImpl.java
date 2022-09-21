2328f6442cec0335c7430bdb0f8fb68623f2fea1
Maksim Ruksha <urpanium@gmail.com>
2022-09-21 12:26:51 +0300

    MP-14 add UserService implementation

---
+++ src/main/java/by/maksimruksha/mapgeneration/service/UserServiceImpl.java
@@ -0,0 +1,60 @@
+package by.maksimruksha.mapgeneration.service;
+
+import by.maksimruksha.mapgeneration.api.repository.UserRepository;
+import by.maksimruksha.mapgeneration.api.service.UserService;
+import by.maksimruksha.mapgeneration.dto.UserDto;
+import by.maksimruksha.mapgeneration.entities.User;
+import by.maksimruksha.mapgeneration.exceptions.ResourceNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    private final ModelMapper mapper;
+
+    @Override
+    public UserDto create(UserDto userDto) {
+        User response = userRepository.save(mapper.map(userDto, User.class));
+        return mapper.map(response, UserDto.class);
+    }
+
+    @Override
+    public UserDto read(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User " + userId + " not found."));
+        return mapper.map(user, UserDto.class);
+    }
+
+    @Override
+    public UserDto update(UserDto userDto) {
+        User user = userRepository.save(mapper.map(userDto, User.class));
+        return mapper.map(user, UserDto.class);
+    }
+
+    @Override
+    public boolean delete(Long userId) {
+        if (userRepository.existsById(userId)) {
+            userRepository.deleteById(userId);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Page<UserDto> findAll(Pageable pageable) {
+        return userRepository
+                .findAll(pageable)
+                .map(user -> mapper.map(user, UserDto.class));
+    }
+
+    @Override
+    public UserDto findById(Long userId) {
+        User response = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User " + userId + " not found."));
+        return mapper.map(response, UserDto.class);
+    }
+}
