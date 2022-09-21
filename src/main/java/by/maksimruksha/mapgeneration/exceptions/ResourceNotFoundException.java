4a82902f3b6c7321337c8a5765ecf956cb1d4b1d
Maksim Ruksha <urpanium@gmail.com>
2022-09-21 08:13:32 +0300

    MP-14 add ResourceNotFoundException

---
+++ src/main/java/by/maksimruksha/mapgeneration/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,13 @@
+package by.maksimruksha.mapgeneration.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message)
+    {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String message, Throwable throwable)
+    {
+        super(message, throwable);
+    }
+}
