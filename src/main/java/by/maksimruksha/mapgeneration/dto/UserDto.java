f68c5ad09cd732e2e6afb6689be2b7b112e360f6
Maksim Ruksha <urpanium@gmail.com>
2022-09-26 02:11:16 +0300

    MP-3 update UserDto to match entity

--- src/main/java/by/maksimruksha/mapgeneration/dto/UserDto.java
+++ src/main/java/by/maksimruksha/mapgeneration/dto/UserDto.java
@@ -1,14 +1,15 @@
 package by.maksimruksha.mapgeneration.dto;
 
+import by.maksimruksha.mapgeneration.security.entities.Role;
 import lombok.Data;
 
 import java.util.List;
+import java.util.Set;
 
 @Data
 public class UserDto {
     private Long id;
     private String name;
-    // private String password;
-    private String role;
+    private Set<Role> roles;
     private List<MapDto> maps;
 }
