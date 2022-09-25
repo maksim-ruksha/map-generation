b3ad1477598fdd374f2db6269757b189b6b48451
Maksim Ruksha <urpanium@gmail.com>
2022-09-26 02:10:50 +0300

    MP-3 change User entity to implement spring's UserDetails

--- src/main/java/by/maksimruksha/mapgeneration/entities/User.java
+++ src/main/java/by/maksimruksha/mapgeneration/entities/User.java
@@ -1,16 +1,21 @@
 package by.maksimruksha.mapgeneration.entities;
 
+import by.maksimruksha.mapgeneration.security.entities.Role;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -21,10 +26,47 @@ public class User {
     @Column(name = "password")
     private String password;
 
-    @Column(name = "role")
-    private String role;
+    @Column(name = "role_id")
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "users_roles", joinColumns = {
+            @JoinColumn(name = "user_id", referencedColumnName = "id")},
+            inverseJoinColumns = {
+                    @JoinColumn(name = "role_id", referencedColumnName = "id")
+            })
+    private Set<Role> roles;
 
     @OneToMany(fetch = FetchType.LAZY)
     @JoinColumn(name = "maps_ids")
     private List<Map> maps;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+
+    @Override
+    public String getUsername() {
+        return name;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
 }
